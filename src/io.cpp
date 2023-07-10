cd75b7b268672e6568d3ff45d20a88833abf899c
Apolline Rodary <apollinerodary@gmail.com>
2023-07-10 13:50:02 +0200

    Shorten loading bars

--- src/io.cpp
+++ src/io.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include "io.hpp"
 
+#define LENGTH 50
+
 using namespace std;
 
 void show_loading_bar(string text, int a, int max) {
-    if (a*100 % max > 0)
+    if (a*LENGTH % max > 0)
         return;
 
     cout << text << " [";
-    for (int i=0; i<100; i++)
+    for (int i=0; i<LENGTH; i++)
         cout << " ";
     cout << "]\r" << text << " [";
-    for (int i=0; i < a*100/max; i++)
+    for (int i=0; i < a*LENGTH/max; i++)
         cout << "=";
     cout << "\r";
     cout.flush();
