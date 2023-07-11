4505ccc632059892b0a74cfc8c15c41a73270861
Apolline Rodary <apollinerodary@gmail.com>
2023-07-11 09:42:55 +0200

    Update Riverswim test and look for bad episodes

--- tests/riverswim.cpp
+++ tests/riverswim.cpp
@@ -8,7 +8,7 @@
 
 #define N 8
 #define SIM_STEPS 1e7
-#define SIM_STEPS_UCRL 1e7
+#define SIM_STEPS_UCRL 1e6
 
 using namespace std;
 namespace plt = matplotlibcpp;
@@ -21,14 +21,18 @@ int main() {
 
     OfflineMDP mdp(actions, transitions, rewards);
 
-    // Find and display optimal policy with value iteration algorithm
-    Policy policy = get<0>(value_iteration(mdp, 1e5, 1e-5));
+    // Find optimal policy and its gain with value iteration algorithm
+    cout << "--- Value iteration" << endl;
+    auto vi_output = value_iteration(mdp, 1e5, 1e-5);
+    Policy policy = get<0>(vi_output);
+    double opt_rewards = get<1>(vi_output);
     show_policy(policy);
-    cout << endl;
+    cout << "Gain is " << opt_rewards << endl << endl;
 
     // Apply policy and estimate invariant measure
+    cout << "--- Invariant measure" << endl;
     Agent agent(mdp, policy);
-    vector<float> d = invariant_measure_estimate(agent, SIM_STEPS);
+    vector<float> d = invariant_measure_estimate(agent, 1e6);
     cout << "Invariant measure after " << SIM_STEPS << " steps is estimated to be:" << endl;
     for (float f: d)
         cout << setw(12) << f << " ";
@@ -40,17 +44,14 @@ int main() {
     for (float f: im)
         cout << setw(12) << f << " ";
     cout << endl << endl;
-
-    // Get gain from invariant measure
-    double opt_rewards = 0.0;
-    for (int i=0; i<N; i++)
-        opt_rewards += im[i]*mdp.getRewards(i, policy(i, 0));
-    cout << "That's a gain of " << opt_rewards << endl << endl;
     
     // Run UCRL2
-    MDP rl_mdp(actions, transitions, rewards);
+    cout << "--- UCRL2" << endl;
+    MDP rl_mdp = (MDP) mdp;
     int duration = SIM_STEPS_UCRL;
-    History ucrl_history = ucrl2(rl_mdp, 1e-5, duration, {});
+    auto ucrl_output = ucrl2(rl_mdp, 1e-5, duration);
+    History history = ucrl_output.first;
+    EpisodeHistory episode_history = ucrl_output.second;
 
     // Compute gap regrets
     double total_rl_rewards=0, total_gap_regret=0;
@@ -64,9 +65,9 @@ int main() {
     vector<double> gap_regrets;
     
     int i=0;
-    for (Event event: ucrl_history) {
+    for (Event event: history) {
         i++;
-        show_loading_bar("Plotting regret...", i, ucrl_history.size());
+        show_loading_bar("Plotting regret... ", i, history.size());
         
         double reward = get<3>(event);
         total_rl_rewards += reward;
@@ -78,11 +79,18 @@ int main() {
         gap_regrets.push_back(total_gap_regret);
     }
 
+    cout << "Waiting for matplotlib..." << endl;
     plt::plot(regrets);
     plt::plot(gap_regrets);
     plt::save("ucrl2_regret.pdf");
 
     cout << endl;
+
+    // Look for a bad episode
+    cout << "--- Observing gain of a suboptimal episode" << endl;
+    auto bad_episode_ucrl_output = seek_bad_episode(mdp, history, episode_history, 1e-5, 1e5); 
+    History bad_episode_history = bad_episode_ucrl_output.first;
+    cout << "Episode lasted " << bad_episode_history.size() << " steps" << endl;
 
     return 0;
 }
